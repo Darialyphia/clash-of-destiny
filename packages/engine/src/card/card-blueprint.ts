0551a1c53a6898fdf523fcab7464e9bc48b31020
loicpennequin <lo.pennequin@gmail.com>
2025-03-04 00:53:36 +0100

    added ability onPlay method

--- packages/engine/src/card/card-blueprint.ts
+++ packages/engine/src/card/card-blueprint.ts
@@ -1,5 +1,9 @@
+import type { Point } from '@game/shared';
+import type { AOEShape } from '../aoe/aoe-shapes';
+import type { Cell } from '../board/cell';
 import type { Game } from '../game/game';
 import type { EffectTarget, SelectedTarget } from '../game/systems/interaction.system';
+import type { Unit } from '../unit/entities/unit.entity';
 import type {
   ArtifactKind,
   CARD_KINDS,
@@ -28,10 +32,18 @@ export type UnitBlueprint = CardBlueprintBase & {
 export type AbilityBlueprint = CardBlueprintBase & {
   kind: Extract<CardKind, typeof CARD_KINDS.ABILITY>;
   manaCost: number;
+  exp: number;
   followup: {
     getTargets(game: Game, card: AbilityCard): EffectTarget[];
     canCommit: (targets: SelectedTarget[]) => boolean;
   };
+  getAoe(game: Game, card: AbilityCard, points: Point[]): AOEShape;
+  onPlay(
+    game: Game,
+    card: AbilityCard,
+    affectedCells: Cell[],
+    affectedUnits: Unit[]
+  ): void;
 };
 
 export type ArtifactBlueprint = CardBlueprintBase & {
--- packages/engine/src/card/entities/ability-card.entity.ts
+++ packages/engine/src/card/entities/ability-card.entity.ts
@@ -1,9 +1,14 @@
 import type { Game } from '../../game/game';
+import type { SelectedTarget } from '../../game/systems/interaction.system';
 import type { Player } from '../../player/player.entity';
 import type { Unit } from '../../unit/entities/unit.entity';
 import type { AbilityBlueprint } from '../card-blueprint';
-import { CARD_KINDS } from '../card.enums';
-import type { CardEventMap } from '../card.events';
+import { CARD_EVENTS, CARD_KINDS } from '../card.enums';
+import {
+  CardAfterPlayEvent,
+  CardBeforePlayEvent,
+  type CardEventMap
+} from '../card.events';
 import { Card, type CardOptions, type SerializedCard } from './card.entity';
 
 export type SerializedAbilityCard = SerializedCard & { kind: typeof CARD_KINDS.ABILITY };
@@ -24,10 +29,55 @@ export class AbilityCard extends Card<
     return this.manaCost <= this.unit.mp;
   }
 
-  play() {}
+  get exp() {
+    return this.blueprint.exp;
+  }
 
   get manaCost() {
     return this.blueprint.manaCost;
+  }
+
+  get followupTargets() {
+    return this.blueprint.followup.getTargets(this.game, this);
+  }
+
+  selectTargets(onComplete: (targets: SelectedTarget[]) => void) {
+    this.game.interaction.startSelectingTargets({
+      player: this.unit.player,
+      getNextTarget: targets => {
+        return (
+          this.blueprint.followup.getTargets(this.game, this)[targets.length] ?? null
+        );
+      },
+      canCommit: this.blueprint.followup.canCommit,
+      onComplete
+    });
+  }
+
+  play() {
+    this.selectTargets(this.playWithTargets.bind(this));
+  }
+
+  playWithTargets(targets: SelectedTarget[]) {
+    const points = targets.map(t => t.cell);
+
+    this.emitter.emit(
+      CARD_EVENTS.BEFORE_PLAY,
+      new CardBeforePlayEvent({ targets: points })
+    );
+
+    const aoeShape = this.blueprint.getAoe(this.game, this, points);
+    this.blueprint.onPlay(
+      this.game,
+      this,
+      aoeShape.getCells(points),
+      aoeShape.getUnits(points)
+    );
+
+    this.emitter.emit(
+      CARD_EVENTS.AFTER_PLAY,
+      new CardAfterPlayEvent({ targets: points })
+    );
   }
 
   serialize(): SerializedAbilityCard {
