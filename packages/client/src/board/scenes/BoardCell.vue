ae8f2327a78fc28b5b90b1f4b97fc1ca5a68e454
loicpennequin <lo.pennequin@gmail.com>
2025-03-11 00:05:26 +0100

    added board cell spawn animation

--- packages/client/src/board/scenes/BoardCell.vue
+++ packages/client/src/board/scenes/BoardCell.vue
@@ -5,11 +5,31 @@ import BoardCellSprite from '@/board/scenes/BoardCellSprite.vue';
 import UiAnimatedSprite from '@/ui/scenes/UiAnimatedSprite.vue';
 import AnimatedIsoPoint from '@/iso/components/AnimatedIsoPoint.vue';
 import BoardCellHighlights from './BoardCellHighlights.vue';
+import { PTransition, type ContainerInst } from 'vue3-pixi';
 
 const { cell } = defineProps<{ cell: SerializedCell }>();
 
 const ui = useBattleUiStore();
 const isHovered = computed(() => ui.hoveredCell?.id === cell.id);
+
+const emit = defineEmits<{ ready: [] }>();
+
+const spawnAnimation = (container: ContainerInst) => {
+  container.y = -400;
+  container.alpha = 0;
+  gsap.to(container, {
+    y: 0,
+    duration: 1,
+    ease: Bounce.easeOut,
+    delay: Math.random() * 0.5,
+    onStart() {
+      container.alpha = 1;
+    },
+    onComplete() {
+      emit('ready');
+    }
+  });
+};
 </script>
 
 <template>
@@ -18,10 +38,16 @@ const isHovered = computed(() => ui.hoveredCell?.id === cell.id);
     @pointerenter="ui.hoverAt(cell.position)"
     @pointerleave="ui.unHover()"
   >
-    <container :ref="(container: any) => ui.assignLayer(container, 'scene')">
-      <BoardCellSprite :cell="cell" />
-      <BoardCellHighlights :cell="cell" />
-      <UiAnimatedSprite assetId="hovered-cell" v-if="isHovered" />
-    </container>
+    <PTransition
+      appear
+      :duration="{ enter: 1000, leave: 0 }"
+      @enter="spawnAnimation"
+    >
+      <container :ref="(container: any) => ui.assignLayer(container, 'scene')">
+        <BoardCellSprite :cell="cell" />
+        <BoardCellHighlights :cell="cell" />
+        <UiAnimatedSprite assetId="hovered-cell" v-if="isHovered" />
+      </container>
+    </PTransition>
   </AnimatedIsoPoint>
 </template>
